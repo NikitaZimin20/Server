1bf5bc232838c66e54eccf3fc2845fac659ae058
Никита Зимин <m1900757@edu.misis.ru>
2021-11-23 20:23:17 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Server
+{
+    public class Program
+    {
+        static ServerObject server; // сервер
+        static Thread listenThread; // потока для прослушивания
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.CreateServer();
+        }
+        private void CreateServer()
+        {
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
